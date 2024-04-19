e77d9b40129b921b373e00cee1f4ed392b8ecb1f
PAF Triarii <pedroarias1015@gmail.com>
2024-04-20 01:19:55 +0200

    Progress initializing the cluster. Succesful init

--- provisioner/entrypoint.sh
+++ provisioner/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ansible-playbook aws/ansible/infra/main.yaml -u ${ANSIBLE_USER_LOCAL} ${VERBOSITY}
-# ansible-playbook aws/ansible/conf/main.yaml -i inventory.ini -u ${ANSIBLE_USER_REMOTE} ${VERBOSITY}
+# ansible-playbook aws/ansible/infra/main.yaml -u ${ANSIBLE_USER_LOCAL} ${VERBOSITY}
+ansible-playbook aws/ansible/conf/main.yaml -i aws/ansible/conf/inventory.ini -u ${ANSIBLE_USER_REMOTE} ${VERBOSITY}
